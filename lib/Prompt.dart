ca0adb419af5f5d632a56e2bc46deba402d778b3
Robert Thurber <83424654+robertthurber@users.noreply.github.com>
2026-02-19 22:08:38 -0500

    added a quit option to the greatings message

--- lib/Prompt.dart
+++ lib/Prompt.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
+//generates a user message and collects users choice
 class Prompt {
   String message() {
-    stdout.write('What would you like to do? (v)iew, (a)dd, (c)heckout: ');
+    stdout.write(
+      'What would you like to do? (v)iew, (a)dd, (c)heckout, (q)uit: ',
+    );
     var userChoice = stdin.readLineSync();
     print(userChoice);
     return userChoice!.trim().toLowerCase();
