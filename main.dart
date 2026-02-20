3ef165f26cb1ab8b974152d69e672f4dd399eae4
Robert Thurber <83424654+robertthurber@users.noreply.github.com>
2026-02-19 21:09:52 -0500

    Initial commit - CLI eCommerce project setup

---
+++ main.dart
@@ -0,0 +1,8 @@
+/* The purpose of this project is to show case what i have learned 
+so far with classes and to make an eCommerce website that can be 
+interacted with in the terminal 
+Version 1.0
+Author Robert Thurber
+ */
+
+void main() {}
